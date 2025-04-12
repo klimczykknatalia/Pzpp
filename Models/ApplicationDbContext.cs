b537a2717eb832f9ec9b6e263d6c0b21651f7d84
Natalia Klimczyk <klimczykknatalia@gmail.com>
2025-04-12 16:18:08 +0200

    Initial project setup with ApplicationDbContext and Contact model

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Pzpp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
